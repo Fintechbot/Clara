01037755adb9f9f053b1eabc5047c41322f49949
Fintechbot <103402546+Fintechbot@users.noreply.github.com>
2022-04-19 14:10:48 +0100

    Update Forcesuscribe.py

--- SaitamaRobot/modules/Forcesuscribe.py
+++ SaitamaRobot/modules/Forcesuscribe.py
@@ -12,9 +12,9 @@ from pyrogram.errors.exceptions.bad_request_400 import (
 )
 from pyrogram.types import ChatPermissions, InlineKeyboardButton, InlineKeyboardMarkup
 
-from AmeliaRobot import DRAGONS as SUDO_USERS
-from AmeliaRobot import pbot
-from AmeliaRobot.modules.sql_extended import forceSubscribe_sql as sql
+from SaitamaRobot import DRAGONS as SUDO_USERS
+from SaitamaRobot import pbot
+from SaitamaRobot.modules.sql_extended import forceSubscribe_sql as sql
 
 logging.basicConfig(level=logging.INFO)
 
