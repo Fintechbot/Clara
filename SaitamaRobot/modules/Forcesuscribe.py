ce243686e1e58e43db5eca3aef8f019e0de72edd
Fintechbot <103402546+Fintechbot@users.noreply.github.com>
2022-04-19 15:14:09 +0100

    Update Forcesuscribe.py

--- SaitamaRobot/modules/Forcesuscribe.py
+++ SaitamaRobot/modules/Forcesuscribe.py
@@ -111,7 +111,7 @@ def _check_member(client, message):
                     )
                 except ChatAdminRequired:
                     sent_message.edit(
-                        "❗ **Amelia is not admin here..**\n__Give me ban permissions and retry.. \n#Ending FSub...__"
+                        "❗ **@fintech01_bot is not admin here..**\n__Give me ban permissions and retry.. \n#Ending FSub...__"
                     )
 
             except ChatAdminRequired:
